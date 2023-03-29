c35c28d71d53278046b371f5d5151275b15e1db0
NIYITANGA Festus <43537970+niyofestus@users.noreply.github.com>
2023-03-29 11:18:13 +0200

    commit app file

--- src/App.js
+++ src/App.js
@@ -1,27 +1,26 @@
 import './App.css';
-
+import Luxury from './components/Luxury';
+import Sedans from './components/Sedans';
+import Suvs from './components/Suvs';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src="Octocat.png" className="App-logo" alt="logo" />
-        <p>
-          GitHub Codespaces <span className="heart">♥️</span> React
-        </p>
-        <p className="small">
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </p>
-      </header>
+    <div className="container">
+      <Sedans heading="SEDAN" par="choose a sedan for 
+      its affordability and exelent fuel economy. Ideal 
+      for cruising in the city or on your next road trip"
+      button="Learn more"
+      />
+      <Suvs heading="SUV" par="Take SUV it's spacious,intirior 
+      and versatality. perfect for your next family vocation and
+      off-road adventures"
+      button="Learn more"
+      />
+      <Luxury
+      heading="LUXURY" par="cruise in the best car brand without
+      the broated prices. Enjoy the enhanced confort of a luxury
+      rental and arrive in style"
+      button="Learn more"
+      />
     </div>
   );
 }
